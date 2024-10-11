4accb7dcf28c93c88523625a096df4c374ac5fda
Florian Weinberger <florian.weinberger@minebeamitsumi.eu>
2024-10-11 12:59:29 +0200

    feat: Initial project setup

--- src/Tasks/general_tasks_include.h
+++ src/Tasks/general_tasks_include.h
@@ -2,4 +2,11 @@
 #ifndef GENERAL_TASKS_INCLUDE_H
 #define GENERAL_TASKS_INCLUDE_H
 
+#include "FreeRTOS.h"
+#include "tasks.h"
+#include "timers.h"
+#include "queue.h"
+#include "semphr.h"
+#include "gpio.h"
+
 #endif //GENERAL_TASKS_INCLUDE_H
